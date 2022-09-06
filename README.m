fa7a1f1e88cecd3d92e5c592ee5d201a30c3e7b3
bzceval <busraaceval@gmail.com>
2022-09-07 02:52:42 +0300

    add readme file in general repo

---
+++ README.m
@@ -0,0 +1,7 @@
+# Diamond
+## This design is the first hands-on project of the Clarusway Me React Ninjas group.
+## Using HTML5 and Css3, the design has been poured into the coding language.
+## Thank you everyone !
+
+### Design is created by Can Öcalır and Busra Ceval on Figma
+[Figma design link]("https://www.figma.com/file/zTm2ezx25QvPqz24zUlcCY/Diamond?node-id=9%3A2")
